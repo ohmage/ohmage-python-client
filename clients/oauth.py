9c23c574548ed37ad8c910cc28586273bb73f4bf
Faisal <falquaddoomi@ucla.edu>
2012-04-26 14:29:06 -0700

    Finally fixed oauth callback issue (hopefully not just for FitBit.)

--- clients/oauth.py
+++ clients/oauth.py
@@ -1,6 +1,7 @@
+import urlparse
 from base import BaseApi
 
-import oauth2 as oauth
+import oauth2
 import urllib, cgi
 
 class OAuthApi(BaseApi):
@@ -36,9 +37,9 @@ class OAuthApi(BaseApi):
         self.api_key = api_key
         self.api_secret = api_secret
 
-        self.consumer = oauth.Consumer(api_key, api_secret)
-        self.client = oauth.Client(self.consumer)
-        self.client.set_signature_method(oauth.SignatureMethod_PLAINTEXT())
+        self.consumer = oauth2.Consumer(api_key, api_secret)
+        self.client = oauth2.Client(self.consumer)
+        self.client.set_signature_method(oauth2.SignatureMethod_PLAINTEXT())
 
         self.request_token_url = request_token_url
         self.access_token_url = access_token_url
@@ -56,24 +57,24 @@ class OAuthApi(BaseApi):
         
         Returns a tuple (rq_token, url) consisting of the request token that was used to
         sign the auth redirect plus the URL to which to redirect. The caller must
-        hold on to the request token and pass it back when calling process_auth()
+        hold on to the request token and pass it back when calling process_auth_response()
         in order to complete the authentication process.
         """
-        
-        # get temporary token
-        resp, content = self.client.request(self.server + self.request_token_url, "GET")
-        if resp['status'] != '200':
-            raise OAuthApi.OAuthException()
 
-        rq_token = dict(cgi.parse_qsl(content))
-        
+        # encode the callback as part of the body, oddly enough
+        extra_params = urllib.urlencode({'oauth_callback': callback_url}) if callback_url else None
+
+        # get temporary token
+        resp, content = self.client.request(self.server + self.request_token_url, "POST", body=extra_params)
+        if resp['status'] != '200':
+            raise OAuthApi.OAuthException("Received a non-200 response (%s) in get_auth_url()" % (resp['status']), content)
+
+        rq_token = dict(urlparse.parse_qsl(content))
+
         # create and return the redirect url
         params = {'oauth_token': rq_token['oauth_token']}
-        
-        if callback_url:
-            params['oauth_callback'] = callback_url
-            
-        return (rq_token, "%s?%s" % (self.server + self.authenticate_url, urllib.urlencode(params)))
+
+        return rq_token, "%s?%s" % (self.server + self.authenticate_url, urllib.urlencode(params))
         
     def process_auth_response(self, rq_token, verifier):
         """
@@ -81,18 +82,18 @@ class OAuthApi(BaseApi):
         from get_auth_url() plus a verifier included in GET parameter 'oauth_verifier'
         of the callback hit that prompted this method to be called.
         """
-        
+
         # use the request token in the session to build a new client.
-        token = oauth.Token(rq_token['oauth_token'], rq_token['oauth_token_secret'])
+        token = oauth2.Token(rq_token['oauth_token'], rq_token['oauth_token_secret'])
         token.set_verifier(verifier)
-        client = oauth.Client(self.consumer, token)
+        client = oauth2.Client(self.consumer, token)
         
         # request the authorized access token
         resp, content = client.request(self.server + self.access_token_url, "POST")
         if resp['status'] != '200':
             raise OAuthApi.OAuthException("Received a non-200 response (%s) in process_auth_response()" % (resp['status']), content)
 
-        access_token = dict(cgi.parse_qsl(content))
+        access_token = dict(urlparse.parse_qsl(content))
         
         return access_token
             
